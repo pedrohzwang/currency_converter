f73c1689085d4a6ed9bdf62ad6cbe7cb650a4371
Pedro Zwang <pedroh.zwang@gmail.com>
2022-09-09 01:42:33 -0300

    init requests

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,25 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+}
+
+void sendRequest() async {
+  Uri url = Uri(host: 'https://api.hgbrasil.com/finance?format=json&key=9c5d7064');
+  HttpClientRequest req = await HttpClient().getUrl(url);
+  HttpClientResponse response = await req.close();
+  // final String = await response.;
+  
 }
